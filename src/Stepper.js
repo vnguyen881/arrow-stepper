a6b1bfb3964da5c587739b09b4f34bfa7318026a
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-10-18 14:19:56 -0400

    update code

--- src/Stepper.js
+++ src/Stepper.js
@@ -3,22 +3,6 @@ import './Stepper.css'; // Import the above CSS
 
 const Stepper = ({ steps, currentStep }) => {
   return (
-    // <div className="steps-container">
-    //   {steps.map((step, index) => {
-    //     console.log('currentStep ', currentStep)
-    //     console.log('step ', step)
-    //     return(
-    //     <div
-    //       key={index}
-    //       className={`steps ${index < currentStep ? 'completed' : ''} ${
-    //         index === currentStep ? 'active' : ''
-    //       }`}
-    //     >
-    //       <span>{step}</span>
-    //     </div>
-    //     )
-    //   })}
-    // </div>
     <ul className="steps-container">
       {steps.map((step, index) => {
         console.log('currentStep ', currentStep)
