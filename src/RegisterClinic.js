cf0d665b69a64acb1e87fb002a3b6e640fa8f1bb
Nguyen <vunguyen64@deloitte.com>
2024-10-21 15:02:27 -0400

    update final style

--- src/RegisterClinic.js
+++ src/RegisterClinic.js
@@ -4,19 +4,19 @@ import Stepper from './Stepper';
 const LogInventory = () => {
   const steps = ['Log Inventory', 'Review'];
   const [currentStep, setCurrentStep] = useState(0);
-  console.log('current step ', currentStep)
+
   return (
     <div>
       <h1>Manually log inventory</h1>
       <Stepper steps={steps} currentStep={currentStep} />
-      <button
-        onClick={() => setCurrentStep(prev => Math.min(prev + 1, steps.length - 1))}
-      >
-        Next Step
-      </button>
-      <button onClick={() => setCurrentStep(prev => Math.max(prev - 1, 0))}>
-        Previous Step
-      </button>
+      <div>
+        <button onClick={() => setCurrentStep(prev => Math.max(prev - 1, 0))}>
+          Previous Step
+        </button>
+        <button onClick={() => setCurrentStep(prev => Math.min(prev + 1, steps.length - 1))}>
+          Next Step
+        </button>
+      </div>
     </div>
   )
 }
